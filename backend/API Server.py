e4ed153dca485ba5ac64333f4f5b1d6924a92913
tau2020 <makombemt@gmail.com>
2024-10-10 23:14:49 +0200

    Add component: API Server

---
+++ backend/API Server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def to_dict(self):
+        return {'id': self.id, 'name': self.name}
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all()
+    return jsonify([item.to_dict() for item in items])
+
+@app.route('/items', methods=['POST'])
+def create_item():
+    data = request.json
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify(new_item.to_dict()), 201
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True, port=5000)
